8ee5b28a4729ddac12affe8d06aa4599d987e7d6
ilgol <iluchka1996@outlook.com>
2017-09-20 21:01:00 +0300

    deleted all files

--- Update/Models/Worker.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Update.Models
-{
-    public class Worker
-    {
-        public string alive { get; set; }
-        public string hashrate { get; set; }
-        public string last_share_timestamp { get; set; }
-    }
-}
